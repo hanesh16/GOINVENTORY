81ebd641a83dd1205a5c7df483a3ea768390449b
Dushyanthbabu <dushyanthbabu525@gmail.com>
2025-11-25 07:01:35 -0500

    Added kafka and update sql query logic

--- queue/kafka.go
+++ queue/kafka.go
@@ -1,1 +1,22 @@
 package queue
+
+import (
+	"fmt"
+
+	"github.com/IBM/sarama"
+)
+
+func ProduceKafkaMessage(topic, message string, producer sarama.SyncProducer) error {
+	messageToSend := &sarama.ProducerMessage{
+		Topic: topic,
+		Value: sarama.StringEncoder(message),
+	}
+
+	partition, offset, err := producer.SendMessage(messageToSend)
+	if err != nil {
+		return err
+	}
+
+	fmt.Printf("Kafka message sent → topic=%s partition=%d offset=%d\n", topic, partition, offset)
+	return nil
+}
